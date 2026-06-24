1713d6bf76d09698ac860afcec988acd69441b64
katemzr <katemzr@gmail.com>
2026-06-24 16:55:39 -0500

    completed group assignment

---
+++ toDoList/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header>
+        <h1>My To-Do List</h1>
+        </header>
+    );     
+}       
---
+++ toDoList/src/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+export default function ToDoList() {
+    return(
+    <div>
+        <ul>
+            <li>Learn React</li>
+            <li>Build a project</li>
+            <li>Read documentation</li>
+        </ul>
+    </div>
+    );
+}
